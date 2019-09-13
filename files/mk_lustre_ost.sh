6335938f614985bc89afa9df9655664bcd999c79
Mika Anttila <mika.anttila@tut.fi>
2019-09-13 15:35:54 +0300

    Some parameter changes to mkfs.lustre

--- files/mk_lustre_ost.sh
+++ files/mk_lustre_ost.sh
@@ -18,23 +18,32 @@ create_journal_fs () {
 	elif ( /usr/bin/lsblk /dev/${VG}/${LV_NAME} --fs -n | grep -q 'OST_.*_jrnl' )
 	then 
 		echo Lustre journal already created to /dev/${VG}/${LV_NAME} >> /dev/stderr
-        exit 0
+        return 0
 	else
 		mkfs -t ext2 -b ${BLOCKSIZE} -O journal_dev -L OST_${INDEX}_jrnl /dev/${VG}/${LV_NAME}
 	fi
+	return 2
 }
 
 create_lustre_fs () {
 	if ( /usr/bin/lsblk ${DEVICE} --fs -n | grep -q 'lustre:OST00' )
 	then
 		echo Lustre fs already created to ${DEVICE} >> /dev/stderr
-        exit 0
+        return 0
 	else
-		/usr/sbin/mkfs.lustre --fsname=lustre --mgsnode=10.2.20.10@o2ib --mgsnode=10.2.20.11@o2ib --ost --index=${INDEX} --mkfsoptions="-E stride=262144,stripe_width=262144 -i 8192 -J device=/dev/${VG}/${LV_NAME}" ${DEVICE}
+		/usr/sbin/mkfs.lustre --replace --fsname=lustre --mgsnode=10.2.20.10@o2ib --mgsnode=10.2.20.11@o2ib --ost --index=${INDEX} --mkfsoptions="-E stride=262144,stripe_width=262144 -i 8192 -J device=/dev/${VG}/${LV_NAME}" ${DEVICE}
 	fi
+	return 2
 }
 create_journal_fs
+JOURNAL=$?
 
 create_lustre_fs
+LDISKFS=$?
 
-exit 2
+if ( $JOURNAL == 2 or $LDISKFS == 2 ) 
+then 
+	exit 2
+else
+	exit 0
+fi
